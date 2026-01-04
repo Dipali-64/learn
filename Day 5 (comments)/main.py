3ec360b4c2fb7c634c536a0867454d75ef9cef47
Dipali-64 <dipalipotle64@gmail.com>
2026-01-04 17:46:49 +0530

    data

---
+++ Day 5 (comments)/main.py
@@ -0,0 +1,19 @@
+# Escape sequence => \n (to print in new line)
+print("Hey i am good girl \nand Jay is also a good boy")   
+
+# multiline comment
+'''
+Hey
+Baby
+'''
+
+# if we want to insert double or single quote in a sentence, we cannot directly insert it..if we do so it will give error
+# The solution is to use this syntax
+#  "text\"text\"text" 
+print("Hey i am a \"good girl\"\nand Anagha is also good girl")
+
+# sep seperates multiple values in same print statement with given character
+# default seperator is space
+# end => specify what to print at the end
+print("Hey",7,9,sep = "~",end="009\n")
+print("Harry")
