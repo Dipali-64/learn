766602b30ab53cb01870312776a2b36d415d517b
Dipali-64 <dipalipotle64@gmail.com>
2026-01-06 19:03:46 +0530

    data

---
+++ Day 6(variables )/main.py
@@ -0,0 +1,31 @@
+
+# a = 12334
+# print(a)
+# a1= 3 
+
+# b = "harry"
+# print(b)
+
+# We cannot concat different type pof data, to concat we have to use same type of data..otherwise it will give error
+# print(a + b)
+
+# print(a+a1)
+
+a = 123
+b = True
+c = "Harry"
+d = None
+
+print("type of a is ",type(a))
+print("type of b is ",type(b))
+print("type of c is ",type(c))
+print("type of d is ",type(d))
+
+list1 = [8,2.3,[-4,5],["apple","banana"]]
+print(list1)
+
+tuple1 =  (("parrot","sparrow"),("Lion","Tiger"))
+print(tuple1)
+
+dict1 = {"name":"Sakshi", "age":20 , "canVote":True}
+print(dict1)
