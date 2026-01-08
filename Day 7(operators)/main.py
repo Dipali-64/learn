b698e0320b157d96f72d769e3862cf8a30b12343
Dipali-64 <dipalipotle64@gmail.com>
2026-01-08 19:07:53 +0530

    data

---
+++ Day 7(operators)/main.py
@@ -0,0 +1,10 @@
+print(15+6)
+print(15-6)
+print(15*6)
+print(15/6)
+# floor division => it removes all values after decimal point
+print(15//6)  
+# modulo division
+print(15%6)
+# exponential
+print(5**3)
---
+++ Day 8(Exercise solution)/main.py
@@ -0,0 +1,10 @@
+# Exercise 1 => create a calculator solution
+# create a calculator capable of performing addition,substraction.multiplication and division on two numbers. your program should format the output in a readable manner ! 
+
+a = 20
+b = 10
+
+print("Addition","=",a + b)
+print("substraction","=",a - b)
+print("Multiplication","=",a * b)
+print("Division","=",a / b)
