3a1fb29dbef16eeffd33e4d97420510de18b6289
Dipali-64 <dipalipotle64@gmail.com>
2026-01-12 17:59:57 +0530

    data

---
+++ Day 10 (user inputs)/main.py
@@ -0,0 +1,15 @@
+# a = input("Enter your name : ")
+# print("my name is : ",a)
+
+x = input("Enter first number : ")
+y = input("Enter second number : ")
+
+print(x + y)
+
+print("Addition of ",x,"and ",y,"is",int(x) + int(y))
+
+print("Addition of ",x,"and ",y,"is",int(x) - int(y))
+
+print("Addition of ",x,"and ",y,"is",int(x) * int(y))
+
+print("Addition of ",x,"and ",y,"is",int(x) / int(y))
