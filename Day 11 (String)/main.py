660d813f2b56a8871d0a3704c857ca0b6b50fb56
Dipali-64 <dipalipotle64@gmail.com>
2026-01-16 18:29:53 +0530

    data

---
+++ Day 11 (String)/main.py
@@ -0,0 +1,29 @@
+name = "Dipali"
+freind = "Anagha"
+anotherFriend = 'Chaitali'
+
+print("Hello," + name)
+
+# apple = "he said, \"I want to eat an appple"
+# apple = "he said, 'I want to eat an apple'"
+# print(apple)
+
+str = ''' he said , 
+hey harry
+hey i am good
+"i want to eat an apple" '''
+
+print(str)
+print(name[0])
+print(name[1])
+print(name[2])
+print(name[3])
+print(name[4])
+print(name[5])
+
+print("let's use a for loop \n")
+for character in name:
+    print(character)
+
+for character in str :
+    print(character)
