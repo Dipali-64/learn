4afc88c38888740a87b4ae629f1c132b7e671f76
Dipali-64 <dipalipotle64@gmail.com>
2026-02-22 10:27:24 +0530

    data

---
+++ Day 16(Match case)/main.py
@@ -0,0 +1,27 @@
+
+x = 15
+# x is the variable to match
+match x:
+    # if x is 0
+    case 0:
+        print("x is zero")
+    # case with if condition
+    case 4 if x%2==0:
+        print("x % 2 == 0 and case is 4")
+    # Empty case with if condition
+    case _ if x<10:
+        print("x is < 10")
+    # default case (will only attached if the above cases were not matched)
+    # so it is basically just an else:
+    case _:
+        print(x)
+
+
+x = int(input("Enter number : "))
+match x:
+    case 0:
+        print("x is 0")
+    case 4:
+        print("x is 4")
+    case _:
+        print(x)
