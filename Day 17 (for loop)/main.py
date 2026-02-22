bfe021156a42eb8aac722a408135814bd9db833e
Dipali-64 <dipalipotle64@gmail.com>
2026-02-22 10:43:30 +0530

    data

---
+++ Day 17 (for loop)/main.py
@@ -0,0 +1,18 @@
+# name = "abhishek"
+# for i in name:
+#     print(i)
+ 
+
+# colors = ["red","green","blue","yellow"]
+# for i in colors:
+#     print(i)
+#     for j in i:
+#         print(j)
+
+for k in range(5):
+    print(k+2)
+
+for a in range(1,9,2):
+    print(a)
+
+# for i in range(start.end,step)
